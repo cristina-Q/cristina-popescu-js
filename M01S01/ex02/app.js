0eb15ceae1896b342c45cbbc79f585289169295b
cristina-q <cristinamihaela@yandex.com>
2020-10-06 21:00:57 +0300

    first commit

--- M01S01/ex02/app.js
+++ M01S01/ex02/app.js
@@ -1,1 +1,20 @@
+let inputLength = document.getElementById('length');
+let inputWidth = document.getElementById('width');
+let elementResult = document.getElementById('result');
+let form = document.querySelector('form');
 
+form.addEventListener(
+  'submit',
+  (e) => {
+    let length = inputLength.value || 0;
+    let width = inputWidth.value || 0;
+    let result = 0;
+
+    result = length * width;
+
+    elementResult.innerText = result;
+
+    e.preventDefault();
+  },
+  false,
+);

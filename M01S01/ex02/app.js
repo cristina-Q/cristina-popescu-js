324b41831a6de911a047ab53a055795d95329052
cristina-q <cristinamihaela@yandex.com>
2020-10-13 18:56:59 +0300

    update

--- M01S01/ex02/app.js
+++ M01S01/ex02/app.js
@@ -1,14 +1,14 @@
-let inputLength = document.getElementById('length');
-let inputWidth = document.getElementById('width');
-let elementResult = document.getElementById('result');
-let form = document.querySelector('form');
+var inputLength = document.getElementById('length');
+var inputWidth = document.getElementById('width');
+var elementResult = document.getElementById('result');
+var form = document.querySelector('form');
 
 form.addEventListener(
   'submit',
-  (e) => {
-    let length = inputLength.value || 0;
-    let width = inputWidth.value || 0;
-    let result = 0;
+  function (e) {
+    var length = inputLength.value || 0;
+    var width = inputWidth.value || 0;
+    var result = 0;
 
     result = length * width;
 
---
+++ M01S03/ex01/app.js
@@ -0,0 +1,17 @@
+console.log(object.age);
+console.log(object.surname);
+
+console.log(
+  'Ma numesc ' +
+    object.name +
+    ' ' +
+    object.surname +
+    ' si am ' +
+    object.age +
+    ' ani.',
+);
+
+// Afiseaza anul in care aveai jumatate din
+// varsta pe care ai trecut-o in obiect scazand
+// din anul curent (un proces numit hardcodare)
+console.log(2020 - object.age / 2);

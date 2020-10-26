d93d0ff88cf69bb0ce88253117c1e2229c9ce4cd
cristina-q <cristinamihaela@yandex.com>
2020-10-26 15:43:56 +0200

    update class

--- M01S04/ex07/app.js
+++ M01S04/ex07/app.js
@@ -24,3 +24,44 @@ let friends = [
     surname: 'Ragnarson',
   },
 ];
+
+console.warn(`1. Folosind o bucla for afiseaza proprietatea
+surname a tuturor obiectelor din arrayul friends.`);
+
+for (let i = 0; i < friends.length; i++) {
+  console.log(friends[i].surname);
+}
+
+console.warn(`2. Afiseaza numele complet al tuturor prietenilor,
+insa o data ce l-ai gasit pe steven, opreste bucla cu
+ajutorul keywordului break.`);
+
+/* for (let i = 0; i < friends.length; i++) {
+  if (friends[i].name === 'Steven') {
+    console.log(friends[i].name, friends[i].surname + `   found steven -> 👨🏻`);
+    break;
+  }
+
+  console.log(friends[i].name, friends[i].surname);
+} */
+
+for (let i = 0; i < friendsLength; i++) {
+  let friend = friends[i];
+  console.log(`${friend.name} ${friend.surname}`);
+
+  if (friend.name === 'Steven' && friend.surname === 'Stevenson') {
+    break;
+  }
+}
+
+console.warn(`3. Folosind keywordul continue, afiseaza numele complet
+al prietenilor, doar daca numarul de caractere total al numelor lor
+este mai mare de 13.`);
+
+for (let i = 0; i < friends.length; i++) {
+  if ((friends[i].name + friends[i].surname).length <= 13) {
+    continue;
+  }
+
+  console.log(friends[i].name, friends[i].surname + `  🎀`);
+}
